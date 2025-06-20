97db927d3882fb7daef0262f16f0aee099d7ad21
DON <praver.gairav@gmail.com>
2025-06-20 15:33:59 +0530

    some components added

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Location from "../../components/Location"
+import Quality_assurance from "../../components/Quality_assurance";
 
 const Contact = () => {
-  return <div>Contact</div>;
-};
-
-export default Contact;
+  return (
+  <div>
+    <p>hello</p>
+    <Location/>
+    contact page
+    <Quality_assurance/>
+  </div>
+  );
+}
